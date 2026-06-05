fcc0cc66be980e7ffd4d49fb2a8d5502fe631a17
kritisingh4256-design <kritisingh4256@gmail.com>
2026-06-05 12:50:57 +0530

    message

--- sumofnnaturalnumber.cpp
+++ sumofnnaturalnumber.cpp
@@ -1,11 +1,11 @@
 # include<iostream>
 using namespace std;
 int main (){
-    int sum, n;
+    int add, n;
     cout<<"enter the value of n";
     cin>> n;
-sum=n*(n+1)/2;
-cout<<"sum of first "<<n<<"  natural numbers ="<<sum;
+add=n*(n+1)/2;
+cout<<"sum of first "<<n<<"  natural numbers ="<<add;
 
 
 
